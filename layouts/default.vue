24f0a4a61a85a25d57a4d389c62d4f24075dbb86
trickdgrit <t3ck17@gmail.com>
2024-10-08 14:28:43 +0800

    penyesuaianmenu

---
+++ layouts/default.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <!-- navbar -->
+    <Navbar />
+
+    <!-- content -->
+    <Nuxt />
+
+    <!-- footer -->
+    <Footer />
+
+  </div>
+</template>
+<script>
+  import Navbar from '@/components/web/navbar.vue'
+  import Footer from '@/components/web/footer.vue'
+  export default {
+    components: {
+      Navbar,
+      Footer
+    },
+  }
+</script>
+
+<style>
+  body {
+    background-color: #ccdcec !important;
+  }
+
+  .jumbotron {
+    background: #186399 !important;
+    color: white;
+  }
+
+  .bg-navbar {
+    background: #12466B;
+  }
+
+  .btn-primary {
+    background-color: #1c669b;
+    border-color: #1c669b;
+  }
+
+  .btn-primary:hover {
+    background-color: #1c669b;
+    border-color: #1c669b;
+  }
+
+  .btn-primary-dark {
+    background-color: #0f456b;
+    border-color: #0f456b;
+    color: white;
+  }
+
+  .btn-primary-dark:hover {
+    color: white;
+  }
+
+  .carousel-item .img-fluid {
+    border-radius: .5rem;
+  }
+
+  .card-category {
+    background-color: #dadada;
+  }
+
+  .object-fit-cover {
+    object-fit: cover;
+  }
+
+  .footer-top {
+    background-color: #186399;
+    color: white;
+  }
+
+  .footer-bottom {
+    background-color: #12466B;
+  }
+</style>
---
+++ pages/admin/user/edit/_id.vue
@@ -0,0 +1,162 @@
+<template>
+  <div class="content-wrapper">
+    <section class="content-header">
+      <div class="container-fluid"></div>
+    </section>
+
+    <section class="content">
+      <div class="card card-outline card-info">
+        <div class="card-header">
+          <h3 class="card-title">
+            <i class="nav-icon fas fa-users"></i> EDIT USER
+          </h3>
+          <div class="card-tools">
+            <button
+              type="button"
+              class="btn btn-tool"
+              data-card-widget="collapse"
+              title="Collapse"
+            >
+              <i class="fas fa-minus"></i>
+            </button>
+            <button
+              type="button"
+              class="btn btn-tool"
+              data-card-widget="remove"
+              title="Remove"
+            >
+              <i class="fas fa-times"></i>
+            </button>
+          </div>
+        </div>
+        <div class="card-body">
+          <form @submit.prevent="updateUser">
+            <div class="form-group">
+              <label>NAMA USER</label>
+              <input
+                type="text"
+                v-model="user.name"
+                placeholder="Masukkan Nama User"
+                class="form-control"
+              />
+              <div v-if="validation.name" class="mt-2">
+                <b-alert show variant="danger">{{
+                  validation.name[0]
+                }}</b-alert>
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label>ALAMAT EMAIL</label>
+              <input
+                type="email"
+                v-model="user.email"
+                placeholder="Masukkan Alamat Email"
+                class="form-control"
+              />
+              <div v-if="validation.email" class="mt-2">
+                <b-alert show variant="danger">{{
+                  validation.email[0]
+                }}</b-alert>
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label>PASSWORD</label>
+              <input
+                type="text"
+                v-model="user.password"
+                placeholder="Masukkan Password"
+                class="form-control"
+              />
+            </div>
+
+            <button class="btn btn-info mr-1 btn-submit" type="submit">
+              <i class="fa fa-paper-plane"></i> UPDATE
+            </button>
+            <button class="btn btn-warning btn-reset" type="reset">
+              <i class="fa fa-redo"></i> RESET
+            </button>
+          </form>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  //layout
+  layout: "admin",
+
+  //meta
+  head() {
+    return {
+      title:
+        "Edit User - TimurRimba.web.id - Belajar Koding Bahasa Indonesia Terlengkap",
+    };
+  },
+
+  data() {
+    return {
+      //state user
+      user: {
+        name: "",
+        email: "",
+        password: "",
+      },
+      //state validation
+      validation: [],
+    };
+  },
+
+  mounted() {
+    //fetching data user by ID
+    this.$axios
+      .get(`/api/admin/users/${this.$route.params.id}`)
+
+      .then((response) => {
+        //assing response data to state "user.name"
+        this.user.name = response.data.data.name;
+
+        //assing response data to state "user.email"
+        this.user.email = response.data.data.email;
+      });
+  },
+
+  methods: {
+    //updateUser method
+    async updateUser() {
+      //sending data to server
+      await this.$axios
+        .put(`/api/admin/users/${this.$route.params.id}`, {
+          //data
+          name: this.user.name,
+          email: this.user.email,
+          password: this.user.password,
+        })
+        .then(() => {
+          //sweet alert
+          this.$swal.fire({
+            title: "BERHASIL!",
+            text: "Data Berhasil Diupdate!",
+            icon: "success",
+            showConfirmButton: false,
+            timer: 2000,
+          });
+
+          //redirect, if success store data
+          this.$router.push({
+            name: "admin-user",
+          });
+        })
+        .catch((error) => {
+          //assign error to state "validation"
+          this.validation = error.response.data;
+        });
+    },
+  },
+};
+</script>
+
+<style></style>
